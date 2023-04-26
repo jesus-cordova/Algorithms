5106832cbe3bf1be4416c499d7adb833fbd900a3
jesus-cordova <jjcordovajj@outlook.com>
2023-04-25 22:52:30 -0400

    first algo push

---
+++ src/main/java/app/arrays/TwoSum.java
@@ -0,0 +1,28 @@
+package app.arrays;
+
+import java.util.Arrays;
+
+class TwoSum {
+    public static void main(String[] args) {
+
+        int[] array = { 15, 7, 2, 11 };
+        int target = 9;
+        System.out.print(Arrays.toString(solveTwoSum(array, target)));
+    }
+
+    public static int[] solveTwoSum(int[] array, int target) {
+        int leftPointer = 0;
+        int rightPointer = array.length - 1;
+        Arrays.sort(array);
+        while (leftPointer != rightPointer) {
+            int currentSum = array[leftPointer] + array[rightPointer];
+            if (currentSum < target)
+                leftPointer++;
+            else if (currentSum > target)
+                rightPointer--;
+            else
+                return new int[] { leftPointer, rightPointer };
+        }
+        return new int[] {};
+    }
+}
